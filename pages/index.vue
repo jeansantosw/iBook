c4a6e1cd0af41ba085216cab3a364864e3441dce
Jean Santos <jeansantos.sw@gmail.com>
2022-02-24 16:36:50 +0000

    #JS #iBook with atomic design

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component">
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  layout: "ibook"
+});
+</script>
+
+<style lang="scss" scoped>
+.component {
+}
+</style>
